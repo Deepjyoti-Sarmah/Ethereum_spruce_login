dd2a4415892695d4dfb1a8a286c307e8cb258061
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2023-03-30 13:45:37 +0530

    making a starter file

---
+++ src/frontend/index.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+
+
+function App() {
+
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />}  />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+const rootNode = document.getElementById('app')
+
+if (rootNode) {
+  const root = ReactDOM.createRoot(rootNode)
+  root.render(<App />)
+}
